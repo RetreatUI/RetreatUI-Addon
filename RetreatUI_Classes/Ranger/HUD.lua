cbbd08c95926d92bd945a8acf675636b51a0e50b
Retreat <gaarn94@gmail.com>
2026-07-31 21:20:10 +0200

    Curate compact Ranger HUD rows

--- RetreatUI_Classes/Ranger/HUD.lua
+++ RetreatUI_Classes/Ranger/HUD.lua
@@ -4,4 +4,38 @@ RUI:RegisterAdvancedClassHUD("Ranger", {
   frameName = "RetreatUIRangerHUD",
   usesPrimaryPower = true,
   supportedLoadouts = {ARCHERY=true,DUELING=true,SURVIVAL=true},
+
+  maxCore = 7,
+  coreIconSize = 36,
+  coreSpacing = 2,
+  coreOrder = {
+    "Incendiary Shot",
+    "Woodland Arrow",
+    "Viper's Bite",
+    "Barbed Shot",
+    "Falconstrike",
+    "Crippling Shot",
+    "Brutal Shot",
+  },
+  strictCoreOrder = false,
+
+  maxUtility = 9,
+  utilityIconSize = 28,
+  utilitySpacing = 2,
+  utilityOrder = {
+    "Throatpunch",
+    "Knockout",
+    "Whipvine Arrow",
+    "Hookshot",
+    "Elude",
+    "Horn of Perseverance",
+    "Horn of War",
+    "Horn of Endurance",
+    "Horn of Alacrity",
+  },
+  strictUtilityOrder = false,
+
+  -- Active horn, Skirmish and class proc icons share one centred row and only
+  -- appear while their aura is active.
+  maxProcs = 6,
 })
