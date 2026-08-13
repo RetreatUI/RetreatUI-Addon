bf7526c9e78da0c3489f05efecae8a32d0dc88bd
Retreat <gaarn94@gmail.com>
2026-08-13 16:31:14 +0200

    beta20: initialize static WA shards

---
+++ RetreatUI/Data/WeakAurasBeta20ShardBridge.lua
@@ -0,0 +1,4 @@
+local RUI = RetreatUI
+if not RUI or type(RUI.Beta20WeakAuras) ~= "table" then return end
+RUI.Beta20WeakAuras.classes = RUI.Beta20WeakAuras.classes or {}
+RUI.NaowhCoAWeakAuras = RUI.Beta20WeakAuras
