ea0d78d0e790e5f1ce5d72a01137f58b59d4a8ac
Retreat <gaarn94@gmail.com>
2026-08-13 20:32:22 +0200

    beta20: delete obsolete WA shard bridge

--- RetreatUI/Data/WeakAurasBeta20ShardBridge.lua
+++
@@ -1,4 +0,0 @@
-local RUI = RetreatUI
-if not RUI or type(RUI.Beta20WeakAuras) ~= "table" then return end
-RUI.Beta20WeakAuras.classes = RUI.Beta20WeakAuras.classes or {}
-RUI.NaowhCoAWeakAuras = RUI.Beta20WeakAuras
