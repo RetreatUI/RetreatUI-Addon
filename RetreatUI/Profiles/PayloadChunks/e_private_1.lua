0483fab76bcb6bc363c3199ba60516bb2e5c84df
Retreat <gaarn94@gmail.com>
2026-08-20 17:23:12 +0200

    beta45: add Edge private ElvUI payload

---
+++ RetreatUI/Profiles/PayloadChunks/e_private_1.lua
@@ -0,0 +1,4 @@
+local RUI = RetreatUI
+if not RUI then return end
+RUI._profilePayloadParts = RUI._profilePayloadParts or {}
+RUI._profilePayloadParts.e_private = (RUI._profilePayloadParts.e_private or "") .. [[!E1!LT8Ykimqmuy43iXUqq6sbDL0nJRJKwouginDysWl9PVZi4fWL5K)pQHUqbLNqsyhw9ck3lG6jNcJqrML6CUMmGdsCz50S6wP4R8mNhrDMchFKYWS78ZpOU3zVk(19hPCfvZzrUompLe4G6A2Tz7(YlQTnLJ3kU1)]]
